835eb4471ed448314384ccb87153de3438a8a06b
ivan <carney520@hotmail.com>
2017-09-07 10:36:01 +0800

    complete EventEmitter doc & test specs

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -2,14 +2,13 @@
  * Event Emitter
  * @module
  */ /** */
-// TODO: 执行回调的过程中可以移除监听
-// TODO: once
 // tslint:disable:no-any
 import { findIndex, debug } from './utils'
 export type Callback = (data: any) => any
 interface ListenerInner {
   callback: Callback
   context: any
+  once: boolean
 }
 
 export interface EventInit {
@@ -58,31 +57,74 @@ export class CustomEvent {
 
 export type Disposer = () => void
 
+/**
+ * 事件订阅器和发送器
+ * @example
+ * <br/>
+ * ```typescript
+ * import { EventEmitter } from 'gzb-jssdk'
+ * 
+ * const emitter = new EventEmitter()
+ * emitter.addListener('foo', data => {
+ *   console.log('foo event emitted', data)
+ * })
+ * 
+ * emitter.emit('foo', 'bar')
+ * // => foo event emitted bar
+ * ```
+ * 
+ */
 export default class EventEmitter {
   private listeners: { [eventType: string]: ListenerInner[] } = {}
+
+  /**
+   * 添加订阅者
+   * @param eventType 事件名
+   * @param callback 回调, 回调中可以返回false将会阻止后续订阅者被执行
+   * @param context 可选，回调执行的上下文
+   * @return 返回一个函数，可以用于移除事件订阅
+   */
   public addListener(
     eventType: string,
     callback: Callback,
     context?: any,
+    once: boolean = false,
   ): Disposer {
     const listeners = this.listeners[eventType] || []
     const listener = {
       callback,
       context,
+      once,
     }
     if (process.env.NODE_ENV === 'development') {
       debug(`[EventEmitter]: addListener ${eventType}`)
     }
     this.listeners[eventType] = listeners.concat(listener)
     return () => {
-      this.removeListener(eventType, callback)
+      return this.removeListener(eventType, callback)
     }
+  }
+
+  /**
+   * 只订阅一次事件, 事件发生后将被移除
+   * @param eventType 事件名
+   * @param callback 回调, 回调中可以返回false将会阻止后续订阅者被执行
+   * @param context 可选，回调执行的上下文
+   * @return 返回一个函数，可以用于移除事件订阅
+   */
+  public once(eventType: string, callback: Callback, context?: any): Disposer {
+    return this.addListener(eventType, callback, context, true)
   }
 
   public getListenerLength(eventType: string) {
     return this.listeners[eventType] ? this.listeners[eventType].length : 0
   }
 
+  /**
+   * 移除指定事件所有订阅者
+   * @param {string} eventType 
+   * @returns 返回被移除的订阅者数量
+   */
   public removeAllListener(eventType: string) {
     if (process.env.NODE_ENV === 'development') {
       debug(`[EventEmitter]: removeAllListener ${eventType}`)
@@ -92,6 +134,13 @@ export default class EventEmitter {
     return length
   }
 
+  /**
+   * 移除订阅者
+   * @param  eventType 
+   * @param  callback 
+   * @param context
+   * @return 返回boolean值，表示是否从队列中被删除
+   */
   public removeListener(eventType: string, callback: Callback, context?: any) {
     if (process.env.NODE_ENV === 'development') {
       debug(`[EventEmitter]: removeListener ${eventType}`)
@@ -110,7 +159,13 @@ export default class EventEmitter {
     return false
   }
 
-  // 这是同步的
+  /**
+   * 触发事件
+   * > 注意：所有回调都是被同步调用的
+   * @param eventType 事件类型
+   * @param data 自定义数据
+   * @returns 返回一个数组，表示所有订阅者回调中返回的值
+   */
   public emit(eventType: string, data?: any) {
     if (process.env.NODE_ENV === 'development') {
       debug(`[EventEmitter]: emit ${eventType}\n ---payload: `, data)
@@ -118,14 +173,19 @@ export default class EventEmitter {
     const listeners = this.listeners[eventType]
     const results = []
     if (listeners && listeners.length) {
+      // copy
+      const _listeners = [...listeners]
       let rushThrough = false
       // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < listeners.length; i++) {
+      for (let i = 0; i < _listeners.length; i++) {
+        const { callback: cb, context, once } = _listeners[i]
+        if (once) {
+          this.removeListener(eventType, cb, context)
+        }
         if (rushThrough) {
           results.push(undefined)
           continue
         }
-        const { callback: cb, context } = listeners[i]
         const rtn = cb.call(context, data)
         // 如果其中一个回调返回false，将停止后面回调的执行
         if (rtn === false) {
--- src/__tests__/EventEmitter.ts
+++ src/__tests__/EventEmitter.ts
@@ -3,9 +3,11 @@
  */
 import EventEmitter from '../EventEmitter'
 
+const eventName = 'test1'
+const eventName2 = 'test2'
+
 describe('test addListener', () => {
   let emitter: EventEmitter
-  const eventName = 'test'
   beforeEach(() => {
     emitter = new EventEmitter()
   })
@@ -20,6 +22,17 @@ describe('test addListener', () => {
     const removed = emitter.removeListener(eventName, cb1)
     expect(removed).toBeTruthy()
     expect(emitter.getListenerLength(eventName)).toBe(1)
+  })
+
+  test('use disposer to remove event listener', () => {
+    const dispose1 = emitter.addListener(eventName, () => {})
+    const dispose2 = emitter.addListener(eventName, () => {})
+
+    expect(emitter.getListenerLength(eventName)).toBe(2)
+    dispose1()
+    expect(emitter.getListenerLength(eventName)).toBe(1)
+    dispose2()
+    expect(emitter.getListenerLength(eventName)).toBe(0)
   })
 
   test('removeAllListener', () => {
@@ -42,5 +55,105 @@ describe('test addListener', () => {
     expect(emitter.getListenerLength(eventName)).toBe(2)
     emitter.removeListener(eventName, ctx.cb, ctx)
     expect(emitter.getListenerLength(eventName)).toBe(1)
+  })
+})
+
+describe('test event emit', () => {
+  let emitter
+  beforeEach(() => {
+    emitter = new EventEmitter()
+  })
+
+  test('addListener', () => {
+    const mcb1 = jest.fn()
+    const mcb2 = jest.fn()
+    const mcb3 = jest.fn()
+    const dispose1 = emitter.addListener(eventName, mcb1)
+    const dispose2 = emitter.addListener(eventName2, mcb2)
+    emitter.emit(eventName, 'foo')
+    expect(mcb1).toBeCalledWith('foo')
+    expect(mcb2).not.toBeCalled()
+    mcb1.mockClear()
+
+    emitter.emit(eventName2, 'bar')
+    expect(mcb1).not.toBeCalled()
+    expect(mcb2).toHaveBeenCalledWith('bar')
+
+    dispose1()
+    emitter.emit(eventName, 'foo')
+    expect(mcb1).not.toBeCalled()
+
+    mcb2.mockClear()
+    emitter.addListener(eventName2, mcb3)
+    dispose2()
+    emitter.emit(eventName2, 'foo')
+    expect(mcb2).not.toBeCalled()
+    expect(mcb3).toBeCalledWith('foo')
+  })
+
+  test('once', () => {
+    const mcb1 = jest.fn()
+    const mcb2 = jest.fn()
+    emitter.once(eventName, mcb1)
+    emitter.addListener(eventName, mcb2)
+
+    emitter.emit(eventName, 'foo')
+    expect(mcb1).toBeCalledWith('foo')
+    expect(mcb2).toBeCalledWith('foo')
+    expect(emitter.getListenerLength(eventName)).toBe(1)
+
+    emitter.emit(eventName, 'bar')
+    expect(mcb1).toHaveBeenCalledTimes(1)
+    expect(mcb2).toHaveBeenCalledTimes(2)
+    expect(mcb1).not.toBeCalledWith('bar')
+    expect(mcb2).toBeCalledWith('bar')
+  })
+
+  test('return false to prevent default and stop call following callbacks', () => {
+    const mcb1 = jest.fn().mockReturnValue('bar')
+    const mcb2 = jest.fn().mockReturnValue(false)
+    const mcb3 = jest.fn()
+    emitter.addListener(eventName, mcb1)
+    emitter.addListener(eventName, mcb2)
+    emitter.addListener(eventName, mcb3)
+
+    const ret = emitter.emit(eventName, 'foo')
+    expect(mcb1).toBeCalledWith('foo')
+    expect(mcb2).toBeCalledWith('foo')
+    expect(mcb3).not.toBeCalled()
+
+    expect(ret).toEqual(['bar', false, undefined])
+  })
+
+  test('addListener in event callback', () => {
+    const mcb2 = jest.fn()
+    const mcb1 = jest.fn(() => {
+      emitter.addListener(eventName, mcb2)
+    })
+
+    emitter.addListener(eventName, mcb1)
+    emitter.emit(eventName, 'foo')
+
+    expect(mcb1).toBeCalledWith('foo')
+    expect(mcb2).not.toBeCalled()
+  })
+
+  test('removeListener in event Callback', () => {
+    const mcb2 = jest.fn()
+    const mcb1 = jest.fn(() => {
+      emitter.removeListener(eventName, mcb2)
+    })
+
+    emitter.addListener(eventName, mcb1)
+    emitter.addListener(eventName, mcb2)
+    emitter.emit(eventName, 'foo')
+
+    expect(mcb1).toBeCalledWith('foo')
+    expect(mcb2).toBeCalledWith('foo')
+
+    emitter.emit(eventName, 'bar')
+    expect(mcb1).toHaveBeenCalledTimes(2)
+    expect(mcb1).toBeCalledWith('bar')
+    expect(mcb2).toHaveBeenCalledTimes(1)
   })
 })
